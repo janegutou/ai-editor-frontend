55225a0b454b9fdb4230f70c57834d54511c4d5a
jane <jiaojie@lenovo.com>
2025-04-22 19:40:00 +0800

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
 
